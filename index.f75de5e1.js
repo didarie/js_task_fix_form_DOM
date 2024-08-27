abad6d6f548ab7f64aac8ddce2736b03c3c8886b
Diana Darienko <diana.darienko21@gmail.com>
2024-08-27 19:52:20 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const inputs = document.querySelectorAll("input");
+for (const input of [
+    ...inputs
+]){
+    input.insertAdjacentHTML("beforebegin", `<label class="field-label" for="${input.id}"></label>`);
+    const labels = document.querySelectorAll("label");
+    for (const label of [
+        ...labels
+    ])label.textContent = input.name;
+    input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
